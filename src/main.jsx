e91ef1679a18c7c69259c4def7b4ab7f61baf83f
hiram.rubio <floreshiram03@gmail.com>
2022-07-13 22:52:32 -0500

    feature: added the about page component and navigation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import MainLayout from './layouts/MainLayout'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MainLayout />
+    <BrowserRouter>
+      <MainLayout />
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
